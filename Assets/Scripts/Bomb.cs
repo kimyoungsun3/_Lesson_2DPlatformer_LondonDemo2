500013baac6881b8686cc3b133496f15614c66ac
kimyoungsun3 <kyssmart@naver.com>
2018-08-13 03:59:54 +0900

    11
    11

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour {
+	public AudioClip boom;
+	public AudioClip fuse;
+	public float bombRadius = 10f;
+	public float bombForce = 100f;
+	public float fuseTime = 1.5f;
+	public GameObject explosion;
+
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/BombPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPickup : MonoBehaviour {
+	public AudioClip pickupClip;
+
+	private Animator animator;
+	private bool bLanded = false;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D _col){
+		//Debug.Log (_col.tag);
+		if (_col.CompareTag ("Player")) {
+			AudioSource.PlayClipAtPoint (pickupClip, transform.position);
+
+			LayBombs _scp = _col.GetComponent<LayBombs> ();
+			if (_scp != null) {
+				_scp.bombCount++;
+			}
+
+			Destroy (gameObject);
+		} else if (_col.CompareTag ("ground") && !bLanded) {
+			bLanded = true;
+			animator.SetTrigger ("Land");
+		}
+	}
+}
