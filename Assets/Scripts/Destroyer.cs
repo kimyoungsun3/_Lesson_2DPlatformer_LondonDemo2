b272610e008910ff2dd2214b94e115851ff1a5ed
kimyoungsun <kyssmart@naver.com>
2018-08-12 23:31:22 +0900

    22
    222

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+	public enum DestroyType { 
+		None,
+		MyDestoryDelay, 
+		ChildDestroyDelay,
+		MyDestoryImmeidate, 
+		ChildDestroyImmeidate
+	};
+
+	public DestroyType type = DestroyType.MyDestoryDelay;
+	public float delayTime;
+	public string strChildName;
+
+	void Awake () {
+		switch(type) 
+		{
+		case DestroyType.None:
+			break;
+		case DestroyType.MyDestoryDelay:
+			Destroy (gameObject, delayTime);
+			break;
+		case DestroyType.ChildDestroyDelay:
+			Destroy(transform.Find(strChildName).gameObject, delayTime);
+			break;
+		case DestroyType.MyDestoryImmeidate:
+			DestroyImmediate (gameObject);
+			break;
+		case DestroyType.ChildDestroyImmeidate:
+			DestroyImmediate(transform.Find(strChildName).gameObject);
+			break;
+		}
+	}
+
+	void DestroyGameObject()
+	{
+		Destroy (gameObject);
+	}
+
+	void DestroyChildGameObject()
+	{
+		if(transform.Find(strChildName).gameObject != null){
+			Destroy(transform.Find(strChildName).gameObject);
+		}
+	}
+
+
+	void DisableChildGameObject()
+	{
+		if(transform.Find(strChildName).gameObject.activeSelf == true ){
+			transform.Find(strChildName).gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+	public GameObject prefabExplosion;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 2f);
+	}
+
+
+	void OnExplode(){
+		Quaternion _q = Quaternion.Euler (0f, 0f, Random.Range (0, 359));
+
+		Instantiate (prefabExplosion, transform.position, _q);
+	}
+
+	void OnTriggerEnter2D(Collider2D _col){
+		if (_col.CompareTag ("Enemy")) {
+			Debug.Log (" > Enemy");
+			_col.gameObject.GetComponent<Enemy> ().Hurt ();
+			OnExplode ();
+			Destroy (gameObject);
+		} else if (_col.CompareTag ("BombPickup")) {
+			Debug.Log (" > BombPickup");
+			_col.gameObject.GetComponent<Bomb>().Explode ();
+			Destroy (_col.transform.root.gameObject);
+			Destroy (gameObject);
+		} else if (!_col.CompareTag ("Player")) {
+			Debug.Log (" > ETC");
+			OnExplode ();
+			Destroy (gameObject);
+		}
+	}
+}
